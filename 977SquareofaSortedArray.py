0682375e315c0e83984762ffe665ba95e874ff00
EricluvPython <59160410+EricluvPython@users.noreply.github.com>
2022-09-03 07:19:28 +0300

    Create 977SquareofaSortedArray.py

---
+++ 977SquareofaSortedArray.py
@@ -0,0 +1,13 @@
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        ans = []
+        l = 0
+        r = len(nums)-1
+        while l <= r:
+            if nums[l]*nums[l] > nums[r]*nums[r]:
+                ans.append(nums[l]*nums[l])
+                l += 1
+            else:
+                ans.append(nums[r]*nums[r])
+                r -= 1
+        return ans[::-1]
